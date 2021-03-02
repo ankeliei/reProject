78adc8cee1b6a6d45ec0fe4c3e3865bfe2993b1a
ankeliei <2533156094@qq.com>
2021-03-02 22:35:48 +0800

    开启有赞之前
    开启有赞之前

--- pages/news/news.js
+++ pages/news/news.js
@@ -5,17 +5,41 @@ Page({
    * 页面的初始数据
    */
   data: {
-
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 设置文章编号
     var that = this
     var newsId = options.newsId
     that.setData({
       newsId: newsId
+    })
+
+    // 加载文章富文本
+    wx.request({
+      url: getApp().globalData.urlPre+"api/getNews.php",
+      header: getApp().globalData.header,
+      data:{
+        id: that.data.newsId
+      },
+      success(res){
+        // 处理头图链接
+        res.data.titlePicture = getApp().globalData.urlPre+"aa_static/" + res.data.titlePicture
+        // 处理内容链接
+        for(var i=0, len = res.data.nodes.length; i < len; i++){
+          if(res.data.nodes[i].kind=="image" || res.data.nodes[i].kind=="video"){
+            res.data.nodes[i].text = getApp().globalData.urlPre+"aa_static/" + res.data.nodes[i].text
+          }
+        }
+        // 写入data
+        that.setData({
+          news: res.data
+        })
+        console.log(res.data)
+      }
     })
   },
 
